6684a3206b229ab85f6b814082bfd99be0b0f7b8
Elias0900 <egarrouj@gmail.com>
2025-01-29 10:19:07 +0100

    ajout bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+
+// Importer les fichiers Bootstrap via Sass
+import './styles/main.scss'  // Ce fichier contient l'import de bootstrap/scss/bootstrap
+
+// Importer les styles globaux de ton projet
 import './style.css'
 
 const app = createApp(App)
+
 app.use(createPinia())
 app.use(router)
+
 app.mount('#app')
